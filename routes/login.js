f457d953e160d698e3fcb59eaed29f0be5e327e8
HakodaBeats <stefan.branila55@gmail.com>
2023-04-17 08:30:40 +0200

    Routes setup

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('home')
+    res.render('login')
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -3,35 +3,47 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-const homeRouter = require('./routes/home')
+// Routes handling
+const home = require('./routes/home')
+app.use('/', home)
 
+const login = require('./routes/login')
+app.use('/login', login)
+
+const register = require('./routes/register')
+app.use('/register', register)
+
+const events = require('./routes/events')
+app.use('/events', events)
+
+app.use(express.static('public'))
 app.set('view engine', 'ejs')
-app.use('/', homeRouter)
 
+// Database handling
 const mariadb = require('mariadb')
 
 const pool = mariadb.createPool({
-    host: 'localhost',
-    user: `${process.env.DATABASE_USER}`,
-    password: `${process.env.DATABASE_PASSWORD}`,
-    connectionLimit: 5
+	host: 'localhost',
+	user: `${process.env.DATABASE_USER}`,
+	password: `${process.env.DATABASE_PASSWORD}`,
+	connectionLimit: 5
 })
 
 let connection
 
 async function dbAsyncConnection() {
-    try {
-        connection = await pool.getConnection()
-        await connection.query("USE TimeModel")
-        console.log('Connessione avvenuta')
+	try {
+		connection = await pool.getConnection()
+		await connection.query("USE TimeModel")
+		console.log('Connessione avvenuta')
 
-    } catch (error) {
-        throw error
+	} catch (error) {
+		throw error
 
-    } finally {
-        if (connection)
-            return connection.end()
-    }
+	} finally {
+		if (connection)
+			return connection.end()
+	}
 }
 
 dbAsyncConnection()
@@ -39,5 +51,6 @@ dbAsyncConnection()
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
+	console.log(`Server listening on port ${port}`)
 })
+
