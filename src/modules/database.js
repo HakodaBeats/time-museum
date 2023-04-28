27e16415d98fedcb33c6e72f55584b8df23f68d9
HakodaBeats <stefan.branila55@gmail.com>
2023-04-28 10:45:27 +0200

    Database module changed

--- src/modules/database.js
+++ src/modules/database.js
@@ -5,13 +5,9 @@ let db = new sqlite3.Database('./database-time.db', err => {
   console.log('Connessione avvenuta con successo')
 })
 
-class HistoryArticle {
-  constructor(article) {
-    this.article = article
-  }
-
-  createArticle() {
-    const { title, subtitle, description, content } = this.article 
+class History {
+  createArticle(article) {
+    const { title, subtitle, description, content } = article 
 
     const query = `
       INSERT INTO History
@@ -21,26 +17,81 @@ class HistoryArticle {
     const stmt = db.prepare(query)
     stmt.run(title, subtitle, description, content)
     stmt.finalize()
+  }
 
-    console.log("Articolo creato con successo!")
+  getArticle(articleID) {
+    const query = `
+      SELECT * FROM History WHERE ArticleID = ?;
+    `
+    const stmt = db.prepare(query)
+    let article = stmt.run(articleID)
+    stmt.finalize()
+
+    return article
+  }
+
+  getAllArticles() {
+    const query = `
+      SELECT * FROM History;
+    `
+    return db.run(query)
+  }
+
+  deleteArticle(articleID) {
+    const query = `
+      DELETE * FROM History WHERE ArticleID = ?
+    `
+    const stmt = db.prepare(query)
+    stmt.run(articleID)
+    stmt.finalize()
+  }
+
+  changeArticle(articleID, newArticle) {
+    this.deleteArticle(articleID)
+    this.createArticle(newArticle)
   }
 }
 
-let firtArticle = new HistoryArticle({
-  title: "Una parola: time",
-  subtitle: "Storia di un museo",
-  description: "Lorem ipsum dolor sit amet",
-  content: "sed do eiusmod tempor incididunt"
+let history = new History()
+
+console.log('Creazione articolo\n')
+
+history.createArticle({
+   title: "Titolo dell'articolo", 
+   subtitle: "Sottititolo bellissimo", 
+   description: "Descrizione meravigliosa", 
+   content: "Contenuto dell'audace articolo"
 })
 
-firtArticle.createArticle()
+console.log('Selezione primo articolo:\n')
 
-db.all('SELECT * FROM History', [], (err, rows) => {
-  if (err) throw err
-  console.log(rows)
+let firstArticle = history.getArticle(1)
+console.log(firstArticle)
+
+console.log('Selezione di tutti gli articoli')
+
+let articles = history.getAllArticles()
+console.log(articles)
+
+console.log('Cambiamento del primo articolo: ')
+
+history.changeArticle(1, {
+   title: "Titolo dell'articolo modificato", 
+   subtitle: "Sottititolo bellissimo modificato", 
+   description: "Descrizione modificata meravigliosa", 
+   content: "Nuovo ontenuto dell'audace articolo"
 })
 
+console.log('Stampa di controllo dopo cambiamento articolo: ')
 
+articles = history.getAllArticles()
+console.log(articles)
 
+console.log('Eliminazione primo articolo')
 
+history.deleteArticle(1)
 
+console.log('Seconda di controllo dopo cambiamento articolo: ')
+
+articles = history.getAllArticles()
+console.log(articles)
